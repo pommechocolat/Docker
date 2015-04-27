8dd8e7855b14e19c2beef3496254c092fc8f8e27
pommechocolat <dev.pommechocolat@free.fr>
2015-04-27 18:25:23 +0200

    Mise en place de script pour démarrer les VMs de façon automatique  avec un dossier en paramètre pour les data des VMs. Un script de stop permet de redémarrer proprement avec le script de start. Mise en place de phpSilex pour une utilisation de la dernière version de PHP et d'Apache. Correction du script php5.3 pour ajouter le module LDAP

---
+++ startDocker.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Author : JLM
+#start boot2docker and container
+
+vmdata=$(pwd)/../vmdata
+
+if [[ $# -eq 1 ]];then
+  vmdata=$1
+fi
+echo $vmdata
+
+#start booot2docker
+boot2docker up
+
+cd php5.3
+docker build -t php53apache22 .
+cd ../phpSilexComposer
+docker build -t phpsilex .
+cd ../mariadb
+docker build -t db_maria_sql .
+cd ../rubyRvm
+docker build -t rvm_maison .
+cd ..
+
+docker images
+
+docker run -td -p 80:80 -p 9000:9000 -v "$vmdata/Sites53":/usr/local/apache2/htdocs -v "$vmdata/logs":/usr/local/apache2/logs --name myphp53 php53apache22
+docker run -td -p 8010:80 -v "$vmdata/Sites":/var/www/html -v "$vmdata/logs":/var/log/apache2 --name myphp phpsilex
+
+docker run -td -p 3306:3306 -v "$vmdata/db_data":/var/lib/mysql --name mydb db_maria_sql
+
+docker run -td -v "$vmdata/dev":/mnt --name myrvm rvm_maison
+
+
+docker ps -a
