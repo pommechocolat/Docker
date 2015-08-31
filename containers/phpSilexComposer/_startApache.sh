6cf0d9407c141de64785580ca5232595ad5194d3
metzger9 <jean-luc.metzger@univ-lorraine.fr>
2015-08-31 15:03:56 +0200

    Ajout de Composer et xDebug à l'image. Cependant il serait sans doute utile et plus modulaire de mettre les outils de dev dans une autre images. De même composer pourrait être ajouter dans une autre image dévolue aux applications construite avec ce système de gestion de paquet PHP.
    Correction de l'IP du conteneur docker utilisé dans le cadre de docker avec Kitematic.

--- containers/phpSilexComposer/_startApache.sh
+++ containers/phpSilexComposer/_startApache.sh
@@ -2,7 +2,7 @@
 
 echo "startup MariaDB / Mysql"
 
-echo "192.168.59.103 sql.lautre.net" >> /etc/hosts
+echo "192.168.99.100 sql.lautre.net" >> /etc/hosts
 echo "<?php phpinfo();" > /var/www/html/info.php
 
 /usr/sbin/apache2ctl -D FOREGROUND
