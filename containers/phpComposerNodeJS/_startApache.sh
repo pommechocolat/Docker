c05803728737ed36b9247b0d168e524f0c7e6939
metzger9 <jean-luc.metzger@univ-lorraine.fr>
2016-01-14 12:39:10 +0100

    Reprise de php5 composer nodeJS pour fonctionner sur baseimage. En espérant que ça démarrera mieux.

--- containers/phpComposerNodeJS/_startApache.sh
+++ containers/phpComposerNodeJS/_startApache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "startup MariaDB / Mysql"
+echo "startup Apache - PHP 5 nodeJS composer..."
 
 echo "192.168.99.100 sql.lautre.net" >> /etc/hosts
 echo "<?php phpinfo();" > /var/www/html/info.php
